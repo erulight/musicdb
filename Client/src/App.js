7dee78a32254d6aa319e4fc1a887de2c685a12d8
Chris Huizenga <cdhuizenga@gmail.com>
2020-02-27 00:35:57 -0500

    added a link on the homepage

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Artist from './Artist';
 import Album from './Album';
 import Song from './Song';
@@ -9,6 +9,11 @@ import Song from './Song';
 function App() {
   return (
     <Router>
+      <Route exact path="/">
+        <div>
+          This will be a real homepage someday, but please <Link to={'/artist/1'}>Click Here</Link>
+        </div>
+      </Route>
       <Route exact path="/artist/:id">
         <Artist />
       </Route>
